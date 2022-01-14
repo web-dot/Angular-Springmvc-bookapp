6588c75800567cdbbc504f8ccd8f75420deaaf3c
web-dot <fedev.sudipto@gmail.com>
2022-01-14 09:26:25 +0530

    created all the methods and performed all the CRUD operations to REST API

--- src/main/java/com/sudip/spring/controller/BookController.java
+++ src/main/java/com/sudip/spring/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,13 @@ public class BookController {
 	public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Book book){
 		bookService.update(id, book);
 		return ResponseEntity.ok().body("Book has been updated");
+	}
+	
+	//Delete a book record
+	@DeleteMapping("/api/book/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") long id){
+		bookService.delete(id);
+		return ResponseEntity.ok().body("Book has been deleted");
 	}
 	
 	
